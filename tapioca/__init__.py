f1ab91ec93d45aa1758e3ae24d6c39f71a23381f
Filipe Ximenes <filipeximenes@gmail.com>
2015-07-09 17:50:07 -0300

    fixing bug in iterator

--- tapioca/__init__.py
+++ tapioca/__init__.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 __author__ = 'Filipe Ximenes'
 __email__ = 'filipeximenes@gmail.com'
-__version__ = '0.2.5'
+__version__ = '0.3.1'
 
 
 from .tapioca import (
