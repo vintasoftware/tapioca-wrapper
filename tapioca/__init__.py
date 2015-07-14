b46d3db0c4c59542c7e9602ee47af726c8770ce8
Filipe Ximenes <filipeximenes@gmail.com>
2015-07-14 17:48:56 -0300

    updating version to 0.3.4

--- tapioca/__init__.py
+++ tapioca/__init__.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 __author__ = 'Filipe Ximenes'
 __email__ = 'filipeximenes@gmail.com'
-__version__ = '0.3.3'
+__version__ = '0.3.4'
 
 
 from .tapioca import (
