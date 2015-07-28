7a6f0750712d527db815ff4e6f5f0d4affcf9873
Filipe Ximenes <filipeximenes@gmail.com>
2015-07-28 17:50:41 -0300

    testing adapter process_response method

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -18,25 +18,30 @@ class TestExceptions(unittest.TestCase):
     def setUp(self):
         self.wrapper = TestTapiocaClient()
 
-    # @responses.activate
-    # def test_adapter_raises_response_process_exception_on_400s(self):
-    #     responses.add(responses.GET, self.wrapper.test().data(),
-    #         body='{"erros": "Bad Request Error"}',
-    #         status=400,
-    #         content_type='application/json')
+    @responses.activate
+    def test_adapter_raises_response_process_exception_on_400s(self):
+        responses.add(responses.GET, self.wrapper.test().data(),
+            body='{"erros": "Server Error"}',
+            status=400,
+            content_type='application/json')
 
-    #     with self.assertRaises(ResponseProcessException):
-    #         TestClientAdapter().process_response(response)
+        response = requests.get(self.wrapper.test().data())
 
-    # @responses.activate
-    # def test_adapter_raises_response_process_exception_on_500s(self):
-    #     responses.add(responses.GET, self.wrapper.test().data(),
-    #         body='{"erros": "Server Error"}',
-    #         status=500,
-    #         content_type='application/json')
+        with self.assertRaises(ResponseProcessException):
+            TestClientAdapter().process_response(response)
 
-    #     with self.assertRaises(ResponseProcessException):
-    #         TestClientAdapter().process_response(response)
+    @responses.activate
+    def test_adapter_raises_response_process_exception_on_500s(self):
+        responses.add(responses.GET, self.wrapper.test().data(),
+            body='{"erros": "Server Error"}',
+            status=500,
+            content_type='application/json')
+
+        response = requests.get(self.wrapper.test().data())
+
+        with self.assertRaises(ResponseProcessException):
+            TestClientAdapter().process_response(response)
+
 
     @responses.activate
     def test_raises_request_error(self):
