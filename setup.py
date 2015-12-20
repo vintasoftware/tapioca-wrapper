3da09a62423615738bc0691c52582d42c06bd071
Filipe Ximenes <filipeximenes@gmail.com>
2015-12-20 14:11:52 -0300

    adding support to python 3.5

--- setup.py
+++ setup.py
@@ -74,10 +74,10 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.1',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
     test_suite='tests',
     tests_require=test_requirements
