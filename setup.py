e39bb7ab8724608a4b0de3a3cf51a86d088ec690
Hugo Bessa <hugo@vinta.com.br>
2022-04-07 15:48:46 -0300

    Upgrades arrow version to 1.x

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ Documentation hosted by Readthedocs: http://tapioca-wrapper.readthedocs.io/en/st
 package = 'tapioca'
 requirements = [
     'requests[security]>=2.6',
-    'arrow>=0.6.0,<1',
+    'arrow<2',
     'six>=1',
     'xmltodict>=0.9.2'
 ]
