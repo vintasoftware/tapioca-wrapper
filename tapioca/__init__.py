9949f4fa46faa38d7160f6f2bd8403ac6cbc851d
Dennys Azevedo <dennys.bd@gmail.com>
2023-06-06 17:38:56 +0000

    set new version

--- tapioca/__init__.py
+++ tapioca/__init__.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 __author__ = 'Filipe Ximenes'
 __email__ = 'filipeximenes@gmail.com'
-__version__ = '2.2.0'
+__version__ = '2.3.0'
 
 
 from .adapters import (
